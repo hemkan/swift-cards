5d9af6940287f80465eae696b77915dc86c95f43
hemkan <hemkano1403@gmail.com>
2024-08-16 19:37:28 -0400

    create new pages for flashcards and flashcard

---
+++ app/flashcard/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Set = () => {
+  return <div>page</div>;
+};
+
+export default Set;
---
+++ app/flashcards/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Flashcards = () => {
+  return <div>page</div>;
+};
+
+export default Flashcards;
